c4e929cd6ced0119179bcc958d78b9e8495d69ae
YYC-0914 <73955061+YYC-0914@users.noreply.github.com>
2021-12-01 19:36:33 +0000

    Create House Robber_198.cpp

---
+++ dynamic_programming/House Robber_198.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(2));
+        // 状态机模型
+        for (int i = 1; i <= n; i ++){
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1] = dp[i-1][0] + nums[i-1];            
+        }
+        return max(dp[n][0], dp[n][1]);    
+    }
+};
