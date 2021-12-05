f3e455db58cd1c2b257377e38f44cc3fcc0ac0d3
YYC-0914 <73955061+YYC-0914@users.noreply.github.com>
2021-12-05 16:42:42 +0000

    Update Minimum Depth of Binary Tree_111.cpp

--- DFS/Minimum Depth of Binary Tree_111.cpp
+++ DFS/Minimum Depth of Binary Tree_111.cpp
@@ -22,3 +22,26 @@ public:
         return dfs(root);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (! root) return 0;
+        if (!root->left  && !root->right) return 1;
+        int res = 0x3f3f3f3f;
+        if (root->left) res = min(res, minDepth(root->left));
+        if (root->right) res = min(res, minDepth(root->right));
+        return res + 1;
+    }
+};
